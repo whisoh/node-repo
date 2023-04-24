10b85be180f7a27d0d3355d2eb89838d3d9a222c
ashank.kumar <ashank.kumar@bonzai.co>
2023-04-24 18:46:46 +0530

    14th commit

---
+++ 1-event-loops-examples/4-server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log('request event')
+    res.end('Hello World')
+})
+
+server.listen(5000, () => {
+    console.log('server listening on port : 5000....')
+})
